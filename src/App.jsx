0d734b714ffd5416cd17d8e25d2a4ae193db7ae9
Tiana-K08 <tetkononenko2020@gmail.com>
2025-12-12 21:10:43 +0200

    feat: add classic React Router configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import './App.css';
+// --- Approach 1: Classic React Router Configuration ---
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {
+import Navbar from './navigation/Navbar.jsx';
+import Homepage from './pages/Homepage.jsx';
+import About from './pages/About.jsx';
+import Contact from './pages/Contact.jsx';
+
+export default function App() {
   return (
-    <>
-      <h1>Hello!</h1>
-    </>
+    <Router>
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/about-me" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+      <footer>{/* <Footer /> */}</footer>
+    </Router>
   );
 }
-
-export default App;
