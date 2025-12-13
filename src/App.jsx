97e25ea2fb50136e1bd02c545f63240034c5f59f
Tiana-K08 <tetkononenko2020@gmail.com>
2025-12-13 19:22:49 +0200

    feat: add JSX-based routing configuration with the Data API

--- src/App.jsx
+++ src/App.jsx
@@ -24,25 +24,52 @@
 //   );
 // }
 
-// --- Approach 2: Object-Based Routing Configuration ---
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+// --- Approach 2: Object-Based Routing Configuration with the Data API ---
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+// import Layout from './navigation/Layout.jsx';
+// import Homepage from './pages/Homepage.jsx';
+// import About from './pages/About.jsx';
+// import Contact from './pages/Contact.jsx';
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout />,
+//     children: [
+//       { index: true, element: <Homepage /> },
+//       { path: 'about-me', element: <About /> },
+//       { path: 'contact', element: <Contact /> },
+//     ],
+//   },
+// ]);
+
+// export default function App() {
+//   return <RouterProvider router={router} />;
+// }
+
+// --- Approach 3: JSX-Based Routing Configuration with the Data API ---
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
 
 import Layout from './navigation/Layout.jsx';
 import Homepage from './pages/Homepage.jsx';
 import About from './pages/About.jsx';
 import Contact from './pages/Contact.jsx';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      { index: true, element: <Homepage /> },
-      { path: 'about-me', element: <About /> },
-      { path: 'contact', element: <Contact /> },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Homepage />} />
+      <Route path="about-me" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+    </Route>
+  )
+);
 
 export default function App() {
   return <RouterProvider router={router} />;
